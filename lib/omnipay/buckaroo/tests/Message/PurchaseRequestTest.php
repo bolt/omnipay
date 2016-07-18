1e77baf42e8527de4dabbf1317002f88d3d0c35e
Gawain Lynch <gawain.lynch@gmail.com>
2016-07-18 12:36:06 +0200

    Update Omnipay components:
      - Updating omnipay/buckaroo (v2.0.2)
      - Updating omnipay/authorizenet (2.4.2)

--- lib/omnipay/buckaroo/tests/Message/PurchaseRequestTest.php
+++ lib/omnipay/buckaroo/tests/Message/PurchaseRequestTest.php
@@ -4,11 +4,16 @@ namespace Omnipay\Buckaroo\Message;
 
 use Omnipay\Tests\TestCase;
 
-class CreditCardPurchaseRequestTest extends TestCase
+class PurchaseRequestTest extends TestCase
 {
+    /**
+     * @var PurchaseRequest
+     */
+    protected $request;
+
     public function setUp()
     {
-        $this->request = new CreditCardPurchaseRequest($this->getHttpClient(), $this->getHttpRequest());
+        $this->request = new PurchaseRequest($this->getHttpClient(), $this->getHttpRequest());
         $this->request->initialize(
             array(
                 'websiteKey' => 'web',
@@ -31,7 +36,8 @@ class CreditCardPurchaseRequestTest extends TestCase
             'returnUrl' => 'https://www.example.com/return',
             'cancelUrl' => 'https://www.example.com/cancel',
             'culture' => 'nl-NL',
-            'paymentMethod' => 'mastercard'
+            'paymentMethod' => 'mastercard',
+            'issuer' => 'RABONL2U',
         ));
 
         $data = $this->request->getData();
@@ -43,31 +49,9 @@ class CreditCardPurchaseRequestTest extends TestCase
         $this->assertSame('https://www.example.com/return', $data['Brq_return']);
         $this->assertSame('https://www.example.com/cancel', $data['Brq_returncancel']);
         $this->assertSame('nl-NL', $data['Brq_culture']);
-        $this->assertSame('mastercard', $data['Brq_payment_method']);
-    }
-    public function testGetDataPaymentMethodFallback()
-    {
-        $this->request->initialize(array(
-            'websiteKey' => 'web',
-            'secretKey' => 'secret',
-            'amount' => '12.00',
-            'currency' => 'EUR',
-            'testMode' => true,
-            'transactionId' => 13,
-            'returnUrl' => 'https://www.example.com/return',
-            'cancelUrl' => 'https://www.example.com/cancel',
-            'culture' => 'nl-NL',
-        ));
 
-        $data = $this->request->getData();
-
-        $this->assertSame('web', $data['Brq_websitekey']);
-        $this->assertSame('12.00', $data['Brq_amount']);
-        $this->assertSame('EUR', $data['Brq_currency']);
-        $this->assertSame(13, $data['Brq_invoicenumber']);
-        $this->assertSame('https://www.example.com/return', $data['Brq_return']);
-        $this->assertSame('https://www.example.com/cancel', $data['Brq_returncancel']);
-        $this->assertSame('nl-NL', $data['Brq_culture']);
-        $this->assertSame('visa,mastercard,amex', $data['Brq_requestedservices']);
+        $this->assertNotContains('Brq_payment_method', $data);
+        $this->assertNotContains('Brq_service_ideal_issuer', $data);
+        $this->assertNotContains('Brq_requestedservices', $data);
     }
 }
