43a7e2d527f77b9ec2b0539639ce48051c228d81
Gawain Lynch <gawain.lynch@gmail.com>
2017-01-04 16:49:26 +0100

    Update components:
      - Updating omnipay/authorizenet (2.4.0 => 2.4.2)
      - Updating omnipay/buckaroo (v2.0.1 => v2.1)
      - Updating omnipay/common (v2.5.0 => 2.5.2)
      - Updating omnipay/dummy (v2.1.1 => v2.1.2)
      - Updating omnipay/manual (v2.2.0 => v2.2.1)
      - Updating omnipay/migs (v2.1.1 => v2.2.1)
      - Updating omnipay/mollie (3.0.5 => v3.1)
      - Updating omnipay/netbanx (v2.1.1 => v2.2)
      - Updating omnipay/payfast (v2.1.1 => v2.1.2)
      - Updating omnipay/payflow (2.2.0 => v2.2.1)
      - Updating omnipay/paypal (v2.5.4 => v2.6.3)
      - Updating omnipay/securepay (v2.1.0 => v2.1.1)
      - Updating omnipay/stripe (v2.3.3 => v2.4.1)

--- lib/omnipay/stripe/src/Message/Response.php
+++ lib/omnipay/stripe/src/Message/Response.php
@@ -17,6 +17,13 @@ use Omnipay\Common\Message\AbstractResponse;
 class Response extends AbstractResponse
 {
     /**
+     * Request id
+     *
+     * @var string URL
+     */
+    protected $requestId = null;
+    
+    /**
      * Is the transaction successful?
      *
      * @return bool
@@ -28,7 +35,9 @@ class Response extends AbstractResponse
 
     /**
      * Get the charge reference from the response of FetchChargeRequest.
-     *
+     * 
+     * @deprecated 2.3.3:3.0.0 duplicate of \Omnipay\Stripe\Message\Response::getTransactionReference()
+     * @see \Omnipay\Stripe\Message\Response::getTransactionReference()
      * @return array|null
      */
     public function getChargeReference()
@@ -300,5 +309,23 @@ class Response extends AbstractResponse
         }
 
         return null;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getRequestId()
+    {
+        return $this->requestId;
+    }
+
+    /**
+     * Set request id
+     *
+     * @return AbstractRequest provides a fluent interface.
+     */
+    public function setRequestId($requestId)
+    {
+        $this->requestId = $requestId;
     }
 }
